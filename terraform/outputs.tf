f640bebf06320a2bee149b0b92efc09723cb68c2
olanak <olana5000@gmail.com>
2026-09-02 20:28:57 +0300

    fix: add log analystics workspace id to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "appgw_public_ip" {
   description = "The public IP address of the Application Gateway"
   value       = azurerm_public_ip.appgw_public_ip.ip_address
 }
+
+output "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics Workspace"
+  value = azurerm_log_analytics_workspace.log_analytics.workspace_id
+}
