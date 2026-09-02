7a8a00a762b2ab282d2cd05bd824f0dfb9a2d42f
olanak <olana5000@gmail.com>
2026-09-02 20:02:45 +0300

    fix: update the ssh key location and the terraform code

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "vm-dvwa" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("${path.module}/id_rsa.pub")
   }
 
   custom_data = filebase64("${path.module}/../scripts/setup_dvwa.sh")
